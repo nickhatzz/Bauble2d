b30f50c510c25ad0e227165cb6b46cd1e004be25
738991 <738991@sylvaniastudents.org>
2025-05-02 17:09:42 -0400

    Initial Commit

---
+++ Bauble2dTests/Bauble2dTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bauble2dTests.swift
+//  Bauble2dTests
+//
+//  Created by Nick on 5/2/25.
+//
+
+import Testing
+@testable import Bauble2d
+
+struct Bauble2dTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Bauble2dUITests/Bauble2dUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Bauble2dUITestsLaunchTests.swift
+//  Bauble2dUITests
+//
+//  Created by Nick on 5/2/25.
+//
+
+import XCTest
+
+final class Bauble2dUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
